02c23d76225058d51b56ff2398d5e5c71b6a2512
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-04-26 01:45:35 +0200

    100-times_table updated 4th

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,36 +1,48 @@
 #include "main.h"
-
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n:number of the times table
+ * print_times_table - prints the times table for n
+ * @n: The multiplication table
+ * Return: Nothing
 */
 void print_times_table(int n)
 {
-int num1, num2, num3;
-
-if (n >= 0 && n <= 15)
-for (num1 = 0; num1 <= n; num1++)
-for (num2 = 0; num2 <= n; num2++)
-num3 = num2 * num1;
-if (num2 == 0)
-_putchar(num3 + '0');
-else if (num3 < 10 && num2 != 0)
+int i, j, times;
+if (!(n > 15 || n < 0))
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+times = (i * j);
+if (j != 0)
+{
 _putchar(',');
 _putchar(' ');
+}
+if (times < 10 && j != 0)
+{
 _putchar(' ');
 _putchar(' ');
-_putchar(num3 + '0');
-else if (num3 >= 10 && num3 < 100)
-_putchar(',');
+_putchar((times % 10) + '0');
+}
+else if (times >= 10 && times < 100)
+{
 _putchar(' ');
-_putchar(' ');
-_putchar((num3 / 10) + '0');
-_putchar((num3 % 10) + '0');
-else if (num3 >= 100)
-_putchar(',');
-_putchar(' ');
-_putchar((num3 / 100) + '0');
-_putchar((num3 / 10) + '0');
-_putchar((num3 % 10) + '0');
-_putchar('\n');
+_putchar((times / 10) + '0');
+_putchar((times % 10) + '0');
+}
+else if (times >= 100 && j != 0)
+{
+_putchar((times / 100) + '0');
+_putchar((times / 10) % 10 + '0');
+_putchar((times % 10) + '0');
+}
+else
+{
+_putchar((times % 10) + '0');
+}
+_putchar('\n')
+}
+}      
+}
 }
