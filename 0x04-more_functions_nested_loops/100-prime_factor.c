8ae370e302ca879452e2e9cdfeeb1f07d0258674
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-04-29 03:12:03 +0200

    100-prime_factor.c updated 6th

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,8 +5,7 @@
  * main - finds and prints the largest prime factor of the number 612852475143
  * followed by a new line
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
 long int n;
@@ -30,10 +29,11 @@ max = i;
 n = n / i;
 }
 }
+
 if (n > 2)
-{
 max = n;
-}
-printf("$ld\n", max);
+
+printf("%ld\n", max);
+
 return (0);
 }
