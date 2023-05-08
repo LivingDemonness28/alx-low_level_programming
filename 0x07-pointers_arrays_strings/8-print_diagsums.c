afe25e6ad5b471ee23cd89317df0318d3e9cafb7
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-05-08 23:24:56 +0200

    0x07 - 8-print_diagsums.c file added

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+int x, y, z;
+
+for (z = 0;  z < size; z++)
+{
+x = x + a[z * size + z];
+}
+for (z = size -1;  z >= 0; z--)
+{
+y += a[z * size + (size -z -1)];
+}
+printf("%d, %d\n", x, y);
+}
