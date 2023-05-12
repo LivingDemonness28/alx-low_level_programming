445fa2f756709a39c3342162d9d4cc8081da0b97
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-05-12 17:28:44 +0200

    0x0A-argc_argv: 3-mul.c updated 2nd

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,18 +12,25 @@ int atoi(const char *s)
 int re = 0, sign = 1, i = 0;
 
 while (str[i] == ' ')
+{
 i++;
+}
 
 if (str[i] == '-')
+{
 sign = -1;
 i++;
+}
 else if (str[i] == '+')
+{
 i++;
+}
 
 while (str[i] >= '0' && str[i] <= '9')
+{
 re = re * 10 + (str[i] - '0')
 i++;
-
+}
 return (re * sign);
 }
 
@@ -39,8 +46,10 @@ int main(int argc, char *argv[])
 int i, j, res;
 
 if (argc != 3)
+{
 printf("Error\n");
 return (1);
+}
 
 i = atoi(argv[1]);
 j = atoi(argv[2]);
