49e9d37c0f3a8da68eb581735b3ba9828e648886
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-05-12 17:01:38 +0200

    0x0A-argc_argv: 3-mul.c file function added to main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,6 +2,7 @@
 #define main_h
 
 int _putchar(char);
+int atoi(const char *str);
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[] __attribute__((unused)));
 int main(int argc, char *argv[]);
