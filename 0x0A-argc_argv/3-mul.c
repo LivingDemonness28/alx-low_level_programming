b836f49a5523e9a2ea36a35340e22cbcf32ade8e
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-05-12 17:48:45 +0200

    0x0A-argc_argv: 3-mul.c updated 6th

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,6 +4,7 @@
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
+ *
  * Return: the int converted from the string
  */
 int _atoi(char *s)
@@ -19,14 +20,16 @@ dig = 0;
 
 while (s[len] != '\0')
 len++;
+
 while (i < len && f == 0)
 {
 if (s[i] == '-')
-++d;
+++j;
+
 if (s[i] >= '0' && s[i] <= '9')
 {
 dig = s[i] - '0';
-if (j % 2)
+if (d % 2)
 dig = -dig;
 n = n * 10 + dig;
 f = 1;
@@ -36,6 +39,7 @@ f = 0;
 }
 i++;
 }
+
 if (f == 0)
 return (0);
 
@@ -46,21 +50,23 @@ return (n);
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
- *
  * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-int result, num1, num2;
+int res, n1, n2;
 
 if (argc < 3 || argc > 3)
 {
 printf("Error\n");
 return (1);
 }
-num1 = _atoi(argv[1]);
-num2 = _atoi(argv[2]);
-result = num1 * num2;
-printf("%d\n", result);
+
+n1 = _atoi(argv[1]);
+n2 = _atoi(argv[2]);
+res = n1 * n2;
+
+printf("%d\n", res);
+
 return (0);
 }
