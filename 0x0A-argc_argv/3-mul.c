f92e2f268c7df80a9b65ecd8e820f15be23b406d
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-05-12 17:50:06 +0200

    0x0A-argc_argv: 3-mul.c updated 7th

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,14 @@
  */
 int _atoi(char *s)
 {
-int i, j, n, len, f, dig;
+int i, d, n, len, f, digit;
 
 i = 0;
-j = 0;
+d = 0;
 n = 0;
 len = 0;
 f = 0;
-dig = 0;
+digit = 0;
 
 while (s[len] != '\0')
 len++;
@@ -24,14 +24,14 @@ len++;
 while (i < len && f == 0)
 {
 if (s[i] == '-')
-++j;
+++d;
 
 if (s[i] >= '0' && s[i] <= '9')
 {
-dig = s[i] - '0';
+digit = s[i] - '0';
 if (d % 2)
-dig = -dig;
-n = n * 10 + dig;
+digit = -digit;
+n = n * 10 + digit;
 f = 1;
 if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
