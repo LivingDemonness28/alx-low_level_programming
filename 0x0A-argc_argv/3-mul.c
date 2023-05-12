5df1e133670e1b2f0c7ccfa014a7fd18dec72b1c
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-05-12 23:32:55 +0200

    0x0A-argc_argv: 3-mul.c updated 11th

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,47 +1,45 @@
 #include "main.h"
 
 /**
- * _atoi - Convert a string to an integer
+ * str_to_integer - Converts a string to an integer
  * @str: The string to convert
  * Return: The converted integer
  */
-int _atoi(const char *str)
+int str_to_integer(char *str)
 {
-int res = 0;
 int sign = 1;
+int result = 0;
 int i = 0;
 
-while (str[i] == ' ')
-i++;
+if (str == NULL)
+return (0);
 
-if (str[i] == '-')
+if (str[0] == '-')
 {
 sign = -1;
 i++;
 }
-else if (str[i] == '+')
+
+for (; str[i] != '\0'; i++)
 {
-i++;
+if (str[i] >= '0' && str[i] <= '9')
+result = result * 10 + (str[i] - '0');
+else
+break;
 }
 
-while (str[i] >= '0' && str[i] <= '9')
-{
-res = res * 10 + (str[i] - '0');
-i++;
-}
-
-return (res * sign);
+return (sign * result);
 }
 
 /**
  * main - Entry point
- * @argc: The number of command-line arguments
- * @argv: An array containing the command-line arguments
+ * @argc: The argument count
+ * @argv: The argument vector
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-int num1, num2, prod;
+int num1, num2, result;
 
 if (argc != 3)
 {
@@ -49,9 +47,10 @@ printf("Error\n");
 return (1);
 }
 
-num1 = _atoi(argv[1]);
-num2 = _atoi(argv[2]);
-prod = num1 * num2;
-printf("%d\n", prod);
+num1 = str_to_integer(argv[1]);
+num2 = str_to_integer(argv[2]);
+result = num1 * num2;
+
+printf("%d\n", result);
 return (0);
 }
