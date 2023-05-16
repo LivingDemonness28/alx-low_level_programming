1f035d67f89de816689e78520b229e27bcee9622
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-05-17 01:55:14 +0200

    0x0B-malloc_free: 2-str_concat.c updated 8th

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,12 +11,14 @@
 char *str_concat(char *s1, char *s2)
 {
 char *cc;
-int i = 0, j = 0;
+int i, j;
 
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
+
+i = j = 0;
 
 while (s1[i] != '\0')
 i++;
