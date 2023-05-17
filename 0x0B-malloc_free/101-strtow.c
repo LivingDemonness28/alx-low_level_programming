7569761dcdffdc095819244350067d1cd01576dd
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-05-17 03:40:40 +0200

    0x0B-malloc_free: 101-strtow.c file updated 15th

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -38,19 +38,21 @@ return (count);
  */
 char **strtow(char *str)
 {
-int i = 0, j = 0, k = 0, word_len = 0, words, l, m;
-char **word_array
-
 if (str == NULL || *str == '\0')
 return NULL;
 
-words = count_words(str);
+int words = count_words(str);
 if (words == 0)
 return NULL;
 
-word_array = malloc((words + 1) * sizeof(char *));
+char **word_array = malloc((words + 1) * sizeof(char *));
 if (word_array == NULL)
 return NULL;
+
+int i = 0;
+int j = 0;
+int k = 0;
+int word_len = 0;
 
 while (str[i] != '\0')
 {
@@ -62,13 +64,13 @@ if (str[i + 1] == ' ' || str[i + 1] == '.' || str[i + 1] == '\n' || str[i + 1] =
 word_array[j] = malloc((word_len + 1) * sizeof(char));
 if (word_array[j] == NULL)
 {
-for (l = 0; l < j; l++)
+for (int l = 0; l < j; l++)
 free(word_array[l]);
 free(word_array);
 return NULL;
 }
 
-for (m = 0; m < word_len; m++, k++)
+for (int m = 0; m < word_len; m++, k++)
 word_array[j][m] = str[k];
 word_array[j][word_len] = '\0';
 j++;
@@ -77,6 +79,8 @@ word_len = 0;
 }
 i++;
 }
+
 word_array[words] = NULL;
+
 return word_array;
 }
