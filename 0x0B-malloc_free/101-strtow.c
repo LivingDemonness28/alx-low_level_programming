11e98f4985eafc1c2306b7e9d24e8939f5ad903d
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-05-17 03:38:46 +0200

    0x0B-malloc_free: 101-strtow.c file updated 14th

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -38,16 +38,17 @@ return (count);
  */
 char **strtow(char *str)
 {
-int i = 0, j = 0, k = 0, word_len = 0;
+int i = 0, j = 0, k = 0, word_len = 0, words, l, m;
+char **word_array
 
 if (str == NULL || *str == '\0')
 return NULL;
 
-int words = count_words(str);
+words = count_words(str);
 if (words == 0)
 return NULL;
 
-char **word_array = malloc((words + 1) * sizeof(char *));
+word_array = malloc((words + 1) * sizeof(char *));
 if (word_array == NULL)
 return NULL;
 
@@ -61,13 +62,13 @@ if (str[i + 1] == ' ' || str[i + 1] == '.' || str[i + 1] == '\n' || str[i + 1] =
 word_array[j] = malloc((word_len + 1) * sizeof(char));
 if (word_array[j] == NULL)
 {
-for (int l = 0; l < j; l++)
+for (l = 0; l < j; l++)
 free(word_array[l]);
 free(word_array);
 return NULL;
 }
 
-for (int m = 0; m < word_len; m++, k++)
+for (m = 0; m < word_len; m++, k++)
 word_array[j][m] = str[k];
 word_array[j][word_len] = '\0';
 j++;
