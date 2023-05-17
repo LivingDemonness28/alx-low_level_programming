64947b90e947d61c6a3b3664a0f5cb569fc1d26d
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-05-17 04:39:36 +0200

    0x0B-malloc_free: 101-strtow.c file updated 37th

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -14,11 +14,11 @@ char **arr1, *x;
 
 if (*str == '\0' || str == NULL)
 return (NULL);
-
 for (i = 0; str[i] != '\0'; i++)
+{
 if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
 w++;
-
+}
 arr1 = (char **)malloc((w + 1) * sizeof(char *));
 if (arr1 == NULL)
 return (NULL);
@@ -32,26 +32,27 @@ for (i = wordf ; str[i] != '\0'; i++)
 if (str[i] == ' ')
 break;
 count++;
-
+}
 *(arr1 + j) = (char *)malloc((count + 1) * sizeof(char));
 if (*(arr1 + j) == NULL)
 {
 for (k = 0; k <= j; k++)
+{
 x = arr1[k];
 free(x);
-
+}
 free(arr1);
 return (NULL);
 }
 for (m = 0; wordf < i; wordf++)
+{
 arr1[j][m] = str[wordf];
 m++;
-
+}
 arr1[j][m] = '\0';
 j++;
 }
 }
 arr1[j] = NULL;
 return (arr1);
-}
 }
