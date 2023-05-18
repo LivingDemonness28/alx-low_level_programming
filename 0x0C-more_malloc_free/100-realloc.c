db0cf0dd4324eff633aa7e4b13c0451f9c2447ef
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-05-19 01:04:11 +0200

    0x0C-more_malloc_free: 100-realloc.c file updated 6th

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,39 +1,46 @@
 #include <stdlib.h>
-#include <string.h>
 #include "main.h"
 
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previously allocated with malloc
- * @old_size: size of the allocated space for ptr in bytes
- * @new_size: new size of the memory block in bytes
- * Return: Pointer to the reallocated memory block
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
+ * Return: pointer to the newly allocated memory block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *np;
-unsigned int min_size;
+char *p1, *op;
+unsigned int i;
 
-if (new_size == 0)
+if (new_size == old_size)
+return (ptr);
+
+if (new_size == 0 && ptr)
 {
 free(ptr);
 return (NULL);
 }
-if (ptr == NULL)
-{
-return (malloc(new_size));
-}
-if (new_size == old_size)
-{
-return (ptr);
-}
-np = malloc(new_size);
 
-if (np != NULL)
+if (!ptr)
+return (malloc(new_size));
+
+p1 = malloc(new_size);
+if (!p1)
+return (NULL);
+
+op = ptr;
+
+if (new_size < old_size)
 {
-min_size = (old_size < new_size) ? old_size : new_size;
-memcpy(np, ptr, min_size);
-free(ptr);
+for (i = 0; i < new_size; i++)
+p1[i] = op[i];
 }
-return (np);
+if (new_size > old_size)
+{
+for (i = 0; i < old_size; i++)
+p1[i] = op[i];
+}
+free(ptr);
+return (p1);
 }
