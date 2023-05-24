af8b9d6d71e36242d84f0aed1ab549f748f1c9fa
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-05-24 16:36:18 +0200

    0x0F-function_pointers: 0-print_name.c updated 26th

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,26 @@
  * print_name - prints a name.
  * @name: input name.
  * @f: function pointer.
- *
  * Return: no return.
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name && f)
-f(name);
+if (name == NULL || f == NULL)
+return;
+
+while (*name != '\0')
+{
+f(*name);
+name++;
+}
+}
+
+/**
+ * print_char - prints a character using putchar.
+ * @c: input character.
+ * Return: no return.
+ */
+void print_char(char *c)
+{
+putchar(*c);
 }
