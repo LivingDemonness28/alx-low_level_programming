1acb0c0dade719a7beb2ba880cddc1f78d2dd4ee
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-05-24 12:54:51 +0200

    0x0F-function_pointers: unused function 0-print_name.c removed from function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,8 +2,6 @@
 #define main_h
 
 void print_name(char *name, void (*f)(char *));
-int main(void);
-void print_name_function(char *name);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
