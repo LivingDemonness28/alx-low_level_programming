4f69be1320299811d88cf3290b6ff2004bcdf44a
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-06-01 23:52:24 +0200

    0x12-singly_linked_lists: 1-list_len.c file updated 9th

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,7 +5,8 @@
  * @h: Pointer to the head of the list
  * Return: Number of elements in the list
 */
-size_t list_len(const list_t *h) {
+size_t list_len(const list_t *h)
+{
 size_t count;
 
 count = 0;
