7db985f63cd9c399996b0aa3514caa2606c9c6e0
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-06-02 01:15:36 +0200

    0x12-singly_linked_lists: 2-add_node.c file updated 4th

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,13 +4,9 @@
 
 /**
  * add_note - Adds a new node at the beginning
- * of a list_t list
- * @head: Pointer to the pointer to the
- * head of the list
- * @str: String to be duplicated and
- * stored in the new node
- * Return: The address of the new element,
- * or NULL if it failedas
+ * @head: head of list
+ * @str: stored string
+ * Return: head address
 */
 list_t *add_node(list_t **head, const char *str)
 {
