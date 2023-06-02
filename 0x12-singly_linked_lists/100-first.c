ab47c8db4ad9e5d6d7796d89c2548821e7073fda
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-06-02 02:15:02 +0200

    0x12-singly_linked_lists: 100-first.c file added

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * before - print a message before
+ * main function is executed
+*/
+void before(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
