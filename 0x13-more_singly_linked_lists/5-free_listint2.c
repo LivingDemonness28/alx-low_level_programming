f510ae987c016309e5b1994598dbdacd21c85629
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-06-05 15:52:30 +0200

    0x13-more_singly_linked_lists: 5-free_listint2.c file added

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *now, *rep = *head;
+
+if (!head)
+{
+return;
+}
+
+while (rep)
+{
+now = rep;
+rep = rep->next;
+free(now);
+}
+*head = NULL;
+}
