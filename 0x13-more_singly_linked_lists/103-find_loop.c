ec949fe4df781e358c62d70ac0e36b7eeb686515
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-06-06 22:11:21 +0200

    0x13-more_singly_linked_lists: 103-find_loop.c file updated 2nd

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,13 +2,14 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - finds the loop in a linked list
+ * find_listint_loop - finds the loop
+ * in a linked list
  * @head: linked list to search for
- *
- * Return: address of the node where the loop starts, or NULL
+ * Return: address of the node where the
+ * loop starts, or NULL
  */
 
-listint_t *find_listint_loop(listint_t *head);
+listint_t *find_listint_loop(listint_t *head)
 {
 const listint_t *slow, *fast;
 size_t apex = 1;
