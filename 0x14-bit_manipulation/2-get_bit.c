b5a0df1fc38eefc212928dc840ac4515b0e70187
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-06-08 17:24:41 +0200

    0x14-bit_manipulation: 2-get_bit.c file updated 2nd

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * set_bit - sets a bit at a given index to 1
+ * @n: pointer to the number to change
+ * @index: index of the bit to set to 1
+ * Return: 1 for success, -1 for failure
+ */
 int get_bit(unsigned int long n, unsigned int index)
 {
 unsigned long int bitMask, res;
