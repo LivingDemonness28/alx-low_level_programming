8d35536b95e8ae66dcb493b828903cd5bae162ca
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-06-12 22:13:49 +0200

    0x15-file_io: 3-cp.c file updated 5th

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,9 +9,9 @@
  * Return: The error message specified by the format
  * string
 */
-void error(int code, const char *format, ...)
+void error_1(int code, const char *format, ...)
 {
-dprintf(STDERR_FILEND, format);
+dprintf(STDERR_FILENO, format);
 exit(code);
 }
 
@@ -32,29 +32,29 @@ ssize_t bytesR = read(ffrom, buffer, BUFFER_SIZE);
 ssize_t bytesW;
 
 if (argc != 3)
-error(97, "Usage: cp file_from file_to\n");
+error_1(97, "Usage: cp file_from file_to\n");
 
 if (ffrom == -1)
-error(98, "Error: Can't read from file %s\n", argv[1]);
+error_1(98, "Error: Can't read from file %s\n", argv[1]);
 
 if (fto == -1)
-error(99, "Error: Can't write to %s\n", argv[2]);
+error_1(99, "Error: Can't write to %s\n", argv[2]);
 
 while (bytesR > 0)
 {
 bytesW = write(fto, buffer, bytesR);
 if (bytesW == -1)
-error(99, "Error: Can't write to %s\n", argv[2]);
+error_1(99, "Error: Can't write to %s\n", argv[2]);
 }
 
 if (bytesR == -1)
-error(98, "Error: Can't read from file %s\n", argv[1]);
+error_1(98, "Error: Can't read from file %s\n", argv[1]);
 
 if (close(ffrom) == -1)
-error(100, "Error: Can't closefd %d\n", ffrom);
+error_1(100, "Error: Can't closefd %d\n", ffrom);
 
 if (close(fto) == -1)
-error(100, "Error: Can't close fd %d\n", fto);
+error_1(100, "Error: Can't close fd %d\n", fto);
 
 return (0);
 }
