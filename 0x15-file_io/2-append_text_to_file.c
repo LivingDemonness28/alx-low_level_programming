68e0883123765907410976dac26d30ffbe4e9b58
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-06-12 21:24:44 +0200

    0x15-file_io: 2-append_text_to_file.c file updated 2nd

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 int file = open(filename, O_WRONLY | O_APPEND);
-ssize_t byesW;
+ssize_t bytesW;
 size_t len;
 
 if (filename == NULL)
