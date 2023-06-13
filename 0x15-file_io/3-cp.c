b8e529f26c2a11654de6d350414139e101f73ce7
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-06-13 15:52:40 +0200

    0x15-file_io: 3-cp.c file updated 13th

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,79 +1,75 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 
 /**
- * error - Print error message and exit
- * with specified code
- * @code: Exit code
- * @format: Error message format string
- * Return: The error message specified by the format
- * string
+ * buffer - Allocates 1024 bytes for a buffer
+ * @file: The name of the file
+ * Return: A pointer to the newly allocated buffer
 */
-void errorExit(int code, const char *format, ...)
+char *buffer(char *file)
 {
-va_list args;
-va_start(args, format);
-vfprintf(stderr, format, args);
-va_end(args);
-exit(code);
+char *buff = malloc(sizeof(char) * 1024);
+
+if (!buff)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+exit(99);
+}
 }
 
 /**
- * main - Copies the content of
- * one file to another file
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
+ * cf - closes file descriptors
+ * @file_desc: The file descriptor to be closed
+*/
+void cf(int file_desc)
+{
+int i = close(file_desc);
+
+if (file_desc == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_desc);
+exit(100);
+}
+}
+
+/**
+ * main - Copies contents of a file to another file
+ * @argc: The number of arguments supplied to a program
+ * @argv: An array of pointers to the arguments
  * Return: 0 (success)
 */
 int main(int argc, char *argv[])
 {
+char *buff = buffer(argv[2]);
 int ffrom = open(argv[1], O_RDONLY);
-int fto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-S_IRUSR | S_IRGRP | S_IWGRP | S_IROTH);
-char buffer[BUFFER_SIZE];
-ssize_t bytesR = read(ffrom, buffer, BUFFER_SIZE);
-ssize_t bytesW;
+int bytesR = read(ffrom, buff, 1024);
+int fto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+int bytesW;
 
-if (argc != 3)
+do
 {
-errorExit(97, "Usage: cp file_from file_to\n");
+if (ffrom == -1 || bytesR == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+free(buff);
+exit(98);
 }
 
-if (ffrom == -1)
+bytesW = write(fto, buff, bytesR);
+if (fto == -1 || bytesW == -1)
 {
-errorExit(98, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+free(buff);
+exit(99);
 }
 
-if (fto == -1)
-{
-errorExit(99, "Error: Can't write to %s\n", argv[2]);
-}
+bytesR = read(ffrom, buff, 1024);
+fto = open(argv[2], O_WRONLY | O_APPEND);
+} while (bytesR > 0);
 
-while (bytesR > 0)
-{
-bytesW = write(fto, buffer, bytesR);
-if (bytesW == -1)
-{
-errorExit(99, "Error: Can't write to %s\n", argv[2]);
-}
-}
-
-if (bytesR == -1)
-{
-errorExit(98, "Error: Can't read from file %s\n", argv[1]);
-}
-
-if (close(ffrom) == -1)
-{
-errorExit(100, "Error: Can't closefd %d\n", ffrom);
-}
-
-if (close(fto) == -1)
-{
-errorExit(100, "Error: Can't close fd %d\n", fto);
-}
-
+free(buff);
+cf(ffrom);
+cf(fto);
 return (0);
 }
