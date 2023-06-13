dabbc523e65bebad11fb8eacf46c3fd2411b815c
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-06-13 17:04:27 +0200

    0x15-file_io: main.h file updated 7th

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,5 +17,16 @@ int append_text_to_file(const char *filename, char *text_content);
 void cf(int file_desc);
 char *buffer(char *file);
 int main(int argc, char *argv[]);
+void check(unsigned char *e_arr);
+void magic_num(unsigned char *e_arr);
+void class_elf(unsigned char *e_arr);
+void data_elf(unsigned char *e_arr);
+void version_elf(unsigned char *e_arr);
+void osabi_elf(unsigned char *e_arr);
+void abi_elf(unsigned char *e_arr);
+void type_elf(unsigned int elf_type, unsigned char *e_arr);
+void entry_elf(unsigned long int elf_entry, unsigned char *e_arr);
+void ce(int elf);
+int main(int __attribute__((__unused__)) argc, char *argv[]);
 
 #endif
