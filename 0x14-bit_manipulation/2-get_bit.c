ef3defac723c9454bb7a23813579fdbdb225788a
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-07-06 15:04:23 +0200

    0x14-bit_manipulation: 2-get_bit.c file added

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit
+ * at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
+ * Return: value of the bit
+ */
+int get_bit(unsigned int long n, unsigned int index)
+{
+unsigned long int bitMask, res;
+
+bitMask = 1UL << index;
+res = n & bitMask;
+
+if (index >= sizeof(n) * 8)
+{
+return (-1);
+}
+
+if (res != 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
