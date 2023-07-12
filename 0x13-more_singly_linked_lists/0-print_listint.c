6df0b3053ddd30fd961a97e271f1af3d9f7d4edf
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-07-12 09:43:51 +0200

    0x13-more_singly_linked_lists: 0-print_listint.c file updated 3rd

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,10 +12,11 @@ size_t print_listint(const listint_t *h)
 size_t count;
 
 count = 0;
-for (; h != NULL; count++)
+while (h != NULL)
 {
 printf("%d\n", h->n);
 h = h->next;
+count++;
 }
 return (count);
 }
