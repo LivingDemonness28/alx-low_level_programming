de0de421d13543d20bf64179eb16e095b63805d2
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-18 01:00:03 +0200

    0x17-doubly_linked_lists: 1-dlistint_len.c file added

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - counts elements in a dlistint list.
+ * @h: head.
+ * Returns: num of elements in list.
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t n = 0
+
+for (; h; n++)
+{
+h = h->next;
+}
+return (n);
+}
