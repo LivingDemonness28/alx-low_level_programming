6207bf8b6fed5e723711c2e17abc96920e2d3d3c
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-22 00:58:23 +0200

    0x18-dynamic_libraries: main.h - _memcpy

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -15,5 +15,6 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
