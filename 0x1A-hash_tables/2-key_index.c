af0abfabf619a8b81bfc42a104accee256aaf18d
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-09-14 22:03:55 +0200

    0x1A-hash_tables: 2-key_index.c updated 2nd

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,8 +9,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-unsigned long int hash = hash_djb2(key);
-unsigned long int index = hash % size;
-
-return (index);
+return (hash_djb2(key) % size);
 }
