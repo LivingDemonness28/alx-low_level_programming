27dc810d02e8fd28730017665897e12bca7670a6
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-09-14 15:32:39 +0200

    0x1A-hash_tables: 3-hash_table_set.c file updated 2nd

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,31 +9,32 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-unsigned long int i = key_index((unsigned char *)key, ht->size);
-hash_node_t *now = ht->array[i];
 hash_node_t *nn;
+hash_node_t *now = strdup (value);
+unsigned long int ind = key_index((const unsigned char *)key, ht->size);
+unsigned long int a = ind;
 
-if (!ht || !key || *key == '\0')
-{
+if (!ht || !key || *key == '\0' || !value)
 return (0);
-}
-while (now)
-{
-if (strcmp(now->key, key) == 0)
-{
-free(now->value);
-now->value = strdup(value);
-if (!(now->value))
-{
+
+if (!now)
 return (0);
-}
+
+while (ht->array[a])
+{
+if (strcmp(ht->array[a]->key, key) == 0)
+{
+free(ht->array[a]->value);
+ht->array[a]->value = now;
 return (1);
 }
-now = now->next;
+a++;
 }
+
 nn = malloc(sizeof(hash_node_t));
-if (nn == NULL)
+if (!nn)
 {
+free(now);
 return (0);
 }
 nn->key = strdup(key);
@@ -42,7 +43,8 @@ if (!(nn->key))
 free(nn);
 return (0);
 }
-nn->next = ht->array[i];
-ht->array[i] = nn;
+nn->value = now;
+nn->next = ht->array[ind];
+ht->array[ind] = now;
 return (1);
 }
